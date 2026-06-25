873766ce6eee6b435a2f4af33b566a306a503891
suzan-sheikh <mdsuzanskh@gmail.com>
2026-06-25 14:46:33 +0600

    client side post complite

--- index.js
+++ index.js
@@ -6,8 +6,24 @@ app.use(cors());
 
 const port = process.env.PORT || 3000;
 
+const users = [
+  { id: 1, name: "sabana", email: "sbana@gmail.com" },
+  { id: 2, name: "sabnur", email: "sbanur@gmail.com" },
+  { id: 3, name: "bobita", email: "bobita@gmail.com" },
+  { id: 4, name: "sabila", email: "sabila@gmail.com" },
+  { id: 5, name: "sabnom", email: "sabnom@gmail.com" },
+  { id: 6, name: "notun", email: "notun@gmail.com" },
+];
+
 app.get("/", (req, res) => {
-  res.send("This server is available");
+  res.send("this server is available");
+});
+app.get("/users", (req, res) => {
+  res.send(users);
+});
+
+app.post("/users", (req, res) => {
+  res.send("user create success");
 });
 
 app.listen(port, () => {
